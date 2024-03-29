99492930385f0fd426104b6f4b89675d22bd3c34
KatliKoma <katlegokoma1@gmail.com>
2024-03-29 02:25:14 +0200

    binary

--- binary_trees.h
+++ binary_trees.h
@@ -62,12 +62,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /*  ------------------ advanced task -----------------------------------*/
- int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
@@ -84,4 +83,5 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int helper(const binary_tree_t *tree, int min, int max);
 #endif /* _BINARY_TREES_H_ */
